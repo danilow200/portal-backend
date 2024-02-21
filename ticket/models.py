7049728b08627d55c28b7cdf2b3225a0f2cd2768
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2024-02-21 10:22:12 -0300

    Adicionando as opções dos campos categoria e prioridade

--- ticket/models.py
+++ ticket/models.py
@@ -2,6 +2,23 @@ import pandas as pd
 import numpy as np
 from django.db import models
 
+CATEGORIAS = (
+    ('DWDM', 'DWDM'),
+    ('FIBRA', 'FIBRA'),
+    ('INFRA', 'INFRA'),
+    ('IP', 'IP'),
+    ('RADIO', 'RADIO'),
+    ('CLIENTE', 'CLIENTE'),
+    ('VIASAT', 'VIASAT'),
+    ('DESCONHECIDO', 'Desconhecido'),
+)
+
+PRIORIDADES = (
+   ('ALTA', 'Alta'),
+   ('MÉDIA', 'Média'),
+   ('BAIXA', 'Baixa'),
+)
+
 class Fila(models.Model):
   nome = models.CharField(max_length=200)
   entrada = models.CharField(max_length=200)
@@ -14,10 +31,10 @@ class Ticket(models.Model):
   ticket = models.IntegerField(primary_key=True)
   estacao = models.CharField(max_length=100)
   descricao = models.CharField(max_length=100)
-  prioridade = models.IntegerField()
+  prioridade = models.CharField(max_length=50, choices=PRIORIDADES)
   sla = models.CharField(max_length=50)
   atendimento = models.CharField(max_length=50)
-  categoria = models.CharField(max_length=50)
+  categoria = models.CharField(max_length=50, choices=CATEGORIAS)
   filas = models.ManyToManyField(Fila)
 
   def __str__(self):
