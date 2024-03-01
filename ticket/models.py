0fe0f6daeb419f65f5c7220c8fa2dc96aa533093
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2024-03-01 09:30:14 -0300

    Impede que o desconto maior que o período seja aplicado

--- ticket/models.py
+++ ticket/models.py
@@ -18,72 +18,81 @@ CATEGORIAS = (
 )
 
 PRIORIDADES = (
-   ('ALTA', 'Alta'),
-   ('MÉDIA', 'Média'),
-   ('BAIXA', 'Baixa'),
+    ('ALTA', 'Alta'),
+    ('MÉDIA', 'Média'),
+    ('BAIXA', 'Baixa'),
 )
 
-class Fila(models.Model):
-  nome = models.CharField(max_length=200)
-  entrada = models.CharField(max_length=200)
-  saida = models.CharField(max_length=200)
 
-  def __str__(self):
-      return self.nome
+class Fila(models.Model):
+    nome = models.CharField(max_length=200)
+    entrada = models.CharField(max_length=200)
+    saida = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
 
 class Ticket(models.Model):
-  ticket = models.IntegerField(primary_key=True)
-  estacao = models.CharField(max_length=100)
-  descricao = models.CharField(max_length=100)
-  prioridade = models.CharField(max_length=50)
-  sla = models.CharField(max_length=50)
-  atendimento = models.CharField(max_length=50)
-  categoria = models.CharField(max_length=50, choices=CATEGORIAS)
-  filas = models.ManyToManyField(Fila)
-  ultimo_desconto_aplicado = models.DurationField(default=timedelta)
+    ticket = models.IntegerField(primary_key=True)
+    estacao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+    prioridade = models.CharField(max_length=50)
+    sla = models.CharField(max_length=50)
+    atendimento = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50, choices=CATEGORIAS)
+    filas = models.ManyToManyField(Fila)
+    ultimo_desconto_aplicado = models.DurationField(default=timedelta)
 
-  def __str__(self):
-      return str(self.ticket)
-  
-  def aplicar_desconto(self, desconto):
-     self.ultimo_desconto_aplicado = desconto
-     self.atendimento = self.atendimento_descontado(desconto)
-     self.save()
+    def __str__(self):
+        return str(self.ticket)
 
-  def reverter_desconto(self, desconto):
-     h, m, s = map(int, self.atendimento.split(':'))
-     atendimento_timedelta = timedelta(hours=h, minutes=m, seconds=s)
+    def aplicar_desconto(self, desconto):
+        h, m, s = map(int, self.atendimento.split(':'))
+        atendimento_timedelta = timedelta(hours=h, minutes=m, seconds=s)
 
-     self.atendimento = str(atendimento_timedelta + desconto)
-     self.save()
-  
-  def atendimento_descontado(self, desconto):
-    h, m, s = map(int, self.atendimento.split(':'))
-    atendimento_timedelta = timedelta(hours=h, minutes=m, seconds=s)
-    
-    return str(atendimento_timedelta - desconto)
-  
+        if desconto > atendimento_timedelta:
+            return
+
+        self.ultimo_desconto_aplicado = desconto
+        self.atendimento = self.atendimento_descontado(desconto)
+        self.save()
+
+    def reverter_desconto(self, desconto):
+        h, m, s = map(int, self.atendimento.split(':'))
+        atendimento_timedelta = timedelta(hours=h, minutes=m, seconds=s)
+
+        self.atendimento = str(atendimento_timedelta + desconto)
+        self.save()
+
+    def atendimento_descontado(self, desconto):
+        h, m, s = map(int, self.atendimento.split(':'))
+        atendimento_timedelta = timedelta(hours=h, minutes=m, seconds=s)
+
+        return str(atendimento_timedelta - desconto)
+
 
 class Desconto(models.Model):
-  inicio = models.DateTimeField(default=timezone.now)
-  fim = models.DateTimeField(default=timezone.now)
-  ticket = models.ForeignKey('Ticket', related_name='descontos', on_delete=models.CASCADE)
+    inicio = models.DateTimeField(default=timezone.now)
+    fim = models.DateTimeField(default=timezone.now)
+    ticket = models.ForeignKey(
+        'Ticket', related_name='descontos', on_delete=models.CASCADE)
 
-  def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
 
-    desconto = self.fim - self.inicio
-    super().save(*args, **kwargs)  # chama o método save original
+        desconto = self.fim - self.inicio
+        super().save(*args, **kwargs)  # chama o método save original
 
-    # Atualiza o campo atendimento do Ticket com o tempo de atendimento descontado
-    self.ticket.aplicar_desconto(desconto)
+        # Atualiza o campo atendimento do Ticket com o tempo de atendimento descontado
+        self.ticket.aplicar_desconto(desconto)
 
-  def delete(self, *args, **kwargs):
-     desconto = self.fim - self.inicio
-     self.ticket.reverter_desconto(desconto)
+    def delete(self, *args, **kwargs):
+        desconto = self.fim - self.inicio
+        self.ticket.reverter_desconto(desconto)
 
-     super().delete(*args, **kwargs)
+        super().delete(*args, **kwargs)
 
-  def __str__(self):
-     return str(self.fim - self.inicio)
+    def __str__(self):
+        return str(self.fim - self.inicio)
 
-  objects = models.Manager()
+    objects = models.Manager()
